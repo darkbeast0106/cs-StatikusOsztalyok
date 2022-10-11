68c71395e83925e8e486db2dd6a17c9ad7486a3d
Budaházi Bence <budahazi.bence@petrik.hu>
2022-10-11 17:43:55 +0200

    2. Feladat elkészítése

--- StatikusOsztalyok/Program.cs
+++ StatikusOsztalyok/Program.cs
@@ -4,7 +4,24 @@
     {
         public static void Main(string[] args)
         {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(Veletlen.VelKarakter('A', 'Z'));
+            }
 
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(Veletlen.velTeljesNev(false));
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(Veletlen.velTeljesNev(true));
+            }
         }
     }
 }
--- StatikusOsztalyok/Veletlen.cs
+++ StatikusOsztalyok/Veletlen.cs
@@ -6,7 +6,77 @@ using System.Threading.Tasks;
 
 namespace StatikusOsztalyok
 {
-    internal class Veletlen
+    internal static class Veletlen
     {
+        private static Random RND = new Random();
+        private static List<string> vezetekNevek = Feltolt("files/veznev.txt");
+        private static List<string> ferfiKeresztNevek = Feltolt("files/ferfikernev.txt");
+        private static List<string> noiKeresztNevek = Feltolt("files/noikernev.txt");
+        private static List<string> Feltolt(string fajlnev)
+        {
+            List<string> list = new List<string>();
+            using (var reader = new StreamReader(fajlnev))
+            {
+                while (!reader.EndOfStream)
+                {
+                    list.Add(reader.ReadLine());
+                }
+            }
+            return list;
+        }
+
+        public static int VelEgesz(int min, int max)
+        {
+            return RND.Next(min, max + 1);
+        }
+
+        public static char VelKarakter(char min, char max)
+        {
+            return (char)VelEgesz(min, max);
+        }
+
+        public static string velVezetekNev()
+        {
+            return vezetekNevek[RND.Next(vezetekNevek.Count)];
+        }
+
+        /// <summary>
+        /// Véletlen keresztnevet generáló függvény
+        /// </summary>
+        /// <param name="nem">A generált keresztnév neme: férfi - false || nő - true</param>
+        /// <returns>Generált véletlen keresztnév</returns>
+        public static string velKeresztNev(bool nem)
+        {
+            string keresztNev;
+            if (nem)
+            {
+                keresztNev = velNoiKeresztNev();
+            } 
+            else
+            {
+                keresztNev = velFerfiKeresztNev();
+            }
+            return keresztNev;
+        }
+
+        private static string velFerfiKeresztNev()
+        {
+            return ferfiKeresztNevek[RND.Next(ferfiKeresztNevek.Count)];
+        }
+
+        private static string velNoiKeresztNev()
+        {
+            return noiKeresztNevek[RND.Next(noiKeresztNevek.Count)];
+        }
+
+        /// <summary>
+        /// Véletlen nevet generáló függvény
+        /// </summary>
+        /// <param name="nem">A generált név neme: férfi - false || nő - true</param>
+        /// <returns>Generált véletlen név</returns>
+        public static string velTeljesNev(bool nem)
+        {
+            return velVezetekNev() + " " + velKeresztNev(nem);
+        }
     }
 }
